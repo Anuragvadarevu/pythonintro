7f3b9b5946f412a54d9c8766033445100385da2e
Anuragvadarevu <52550909+Anuragvadarevu@users.noreply.github.com>
2020-02-26 21:38:26 -0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,14 @@
+sports=['cricket','badminton','squash','racquetball','tennis']
+
+
+print(sports)
+
+print(len(sports))
+
+
+for i in range(len(sports)) :
+	print(i,sports[i])
+	
+#replace position 2 with  'football	
+sports[2]='football'
+print(sports)
